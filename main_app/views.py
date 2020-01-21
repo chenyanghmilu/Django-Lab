8fc6a3b2091349cf28b560f32ea120c7e2fb018c
Yang Chen <chenyanghmilu@gmail.com>
2020-01-20 23:08:23 -0500

    add models and admin

--- main_app/views.py
+++ main_app/views.py
@@ -1,26 +1,16 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-
-class Finch:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-finches = [
-    Finch('Name1', 'tabby', 'foul little demon', 3),
-    Finch('Name2', 'tortoise shell', 'diluted tortoise shell', 0),
-    Finch('Name3', 'black tripod', '3 legged cat', 4)
-]
-
+from .models import Finch
 
 def home(request):
-    return HttpResponse('<h1>Hello Finches</h1>')
+    return render(request, 'home.html')
 
 def about(request):
     return render(request, 'about.html')
 
 def finches_index(request):
+    finches = Finch.objects.all()
     return render(request, 'finches/index.html', { 'finches': finches })
+
+def finches_detail(request, finch_id):
+    finch = Finch.objects.get(id=finch_id)
+    return render(request, 'finches/detail.html', { 'finch': finch })
