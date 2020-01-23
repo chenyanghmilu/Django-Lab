31f87ffd5392f6e430ddbe68129e2585fcea7bd0
Yang Chen <chenyanghmilu@gmail.com>
2020-01-22 23:34:43 -0500

    add aws3 to upload photo

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
-from .models import Finch, Toy
+from .models import Finch, Toy, Photo
 from .forms import FeedingForm
+import uuid
+import boto3
+
+S3_BASE_URL = 'https://s3-us-east-2.amazonaws.com/'
+BUCKET = 'finchcollectors'
 
 class FinchCreate(CreateView):
     model = Finch
@@ -46,6 +51,26 @@ def add_feeding(request, finch_id):
         new_feeding = form.save(commit=False)
         new_feeding.finch_id = finch_id
         new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
+
+def add_photo(request, finch_id):
+    # photo-file was the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + \
+            photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+        # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+        # we can assign to finch_id or finch (if you have a finch object)
+            photo = Photo(url=url, finch_id=finch_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
     return redirect('detail', finch_id=finch_id)
 
 def assoc_toy(request, finch_id, toy_id):
